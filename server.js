49a5294b1f11a2e3ca677ca60c3adf3628deca22
przemekSzafert <szafert.p@gmail.com>
2020-02-10 20:31:43 +0100

    vue cli & server.js setup

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const Post = require("./models/Post");
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log("DB connected"))
   .catch(err => console.error(err));
